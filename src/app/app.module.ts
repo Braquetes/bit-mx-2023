e5f07916ceab1307735d5d9b5a7f89c9e0578d1d
Rodolfo Braquetes <braquetesss@gmail.com>
2022-11-05 23:05:48 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,55 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { CodigoComponent } from './pages/codigo/codigo.component';
+import { RestaurarComponent } from './pages/restaurar/restaurar.component';
+import { PasswordComponent } from './pages/password/password.component';
+import { HomeComponent } from './client/home/home.component';
+import { PerfilComponent } from './client/perfil/perfil.component';
+import { PlanesComponent } from './client/planes/planes.component';
+import { QrComponent } from './client/qr/qr.component';
+import { ProgramaComponent } from './client/programa/programa.component';
+import { TalleresComponent } from './client/talleres/talleres.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { NavbarClientComponent } from './shared/navbar-client/navbar-client.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
+import { CompletarComponent } from './client/completar/completar.component';
+import { MetodoComponent } from './client/metodo/metodo.component';
+import { EleccionComponent } from './client/eleccion/eleccion.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent,
+    RegistroComponent,
+    CodigoComponent,
+    RestaurarComponent,
+    PasswordComponent,
+    HomeComponent,
+    PerfilComponent,
+    PlanesComponent,
+    QrComponent,
+    ProgramaComponent,
+    TalleresComponent,
+    NavbarComponent,
+    NavbarClientComponent,
+    FooterComponent,
+    CompletarComponent,
+    MetodoComponent,
+    EleccionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
